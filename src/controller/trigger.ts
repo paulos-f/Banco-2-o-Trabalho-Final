c6acd9eed5d146b242874a61839ad0363fd22c3d
paulos-f <133170300+paulos-f@users.noreply.github.com>
2024-06-25 11:57:55 -0300

    Update trigger.ts

--- src/controller/trigger.ts
+++ src/controller/trigger.ts
@@ -1,1 +1,13 @@
-t
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+class ComandoDML {
+  public async triggers(req: Request, res: Response) {
+    try {
+        
+    } catch (error) {
+      return res.send(error)
+    }
+  }
