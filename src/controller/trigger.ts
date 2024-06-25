81916b2699c830d95055a6ab7640f7e6b11e7df0
paulos-f <133170300+paulos-f@users.noreply.github.com>
2024-06-25 13:49:59 -0300

    1

--- src/controller/trigger.ts
+++ src/controller/trigger.ts
@@ -1,38 +1,32 @@
-import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
-
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-class ComandoDML {
-  public async triggers(req: Request, res: Response) {
-    try {
-        prisma.$use(async (params, next) => {
-        if (params.model === 'Cliente' && params.action === 'create') {
-        const result = await next(params);
-        
-        // Após a inserção, insere no historico_clien
-        await prisma.historicoClien.create({
-          data: {
-            dt_insercao: new Date(),
-            nm_usuario: user,
-            cod_cliente: result.cod_cliente,
-            cpf: result.cpf,
-            nome: result.nome,
-            sexo: result.sexo,
-            dt_nascimento: result.dt_nascimento,
-            dt_cadastro: result.dt_cadastro,
-            num_contato: result.num_contato,
-            email: result.email,
-            endereco: result.endereco
-          },
-        });
-        console.log('Novos dados inseridos no historico_clien');
-        return result;
-      } else {
-        return next(params);
-      }
-    })
-    } catch (error) {
-      return res.send(error)
-    }
+// Middleware para capturar inserções na tabela Cliente
+prisma.$use(async (params, next) => {
+  if (params.model === 'Cliente' && params.action === 'create') {
+    const result = await next(params);
+
+    const user = 'admin'
+
+    // Após a inserção, insere no historico_clien
+    await prisma.historicoClien.create({
+      data: {
+        dt_insercao: new Date(),
+        nm_usuario: user,
+        cod_cliente: result.cod_cliente,
+        cpf: result.cpf,
+        nome: result.nome,
+        sexo: result.sexo,
+        dt_nascimento: result.dt_nascimento,
+        dt_cadastro: result.dt_cadastro,
+        num_contato: result.num_contato,
+        email: result.email,
+        endereco: result.endereco
+      },
+    });
+    console.log('Novos dados inseridos no historico_clien');
+    return result;
+  } else {
+    return next(params);
   }
+});
